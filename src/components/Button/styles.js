15c7f105629003cdee8bdbdc82f9f7a0661e3f2c
Nicolas Ferreira <74085306+Nicolas-FSantos@users.noreply.github.com>
2024-11-07 17:39:35 -0300

    feat: basic operation functions
    Added basic math operations

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+
+    border: 1px solid #c3d9d7;
+    background-color: #6999db;
+    color: white;
+    font-size: 24px;
+    font-weight:700;
+    flex: 1;
+    border-radius: 10px;
+
+    &:hover{
+        opacity: 0.6;
+    }
+
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+
+    margin-bottom: 5px;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #121212;
+        border: 0;
+        display: flex;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: white;
+        border-radius: 10px;
+    }
+`
